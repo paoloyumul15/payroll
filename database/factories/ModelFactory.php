d07af2bfe4b5ae15cba650c1d4e22f59c53b3e4e
Erwin Paolo Yumul <paolo.yumul@itechsystemsolutions.com>
2017-03-18 10:16:24 +0800

    Added simple database migrations and seeders

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,24 +1,43 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
-
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
+use App\Models\Profile;
+use App\User;
 
+$factory->define(User::class, function (Faker\Generator $fake) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'employee_id' => $fake->unique()->numberBetween(10000, 99999),
+        'first_name' => $fake->firstName,
+        'middle_name' => $fake->lastName,
+        'last_name' => $fake->lastName,
+        'email' => $fake->unique()->safeEmail,
+        'password' => bcrypt('secret'),
         'remember_token' => str_random(10),
+        'created_at' => date('Y-m-d H:i:s'),
+        'updated_at' => date('Y-m-d H:i:s'),
+    ];
+});
+
+$factory->define(Profile::class, function (Faker\Generator $fake) {
+    return [
+        'user_id' => function() {
+            return factory(User::class)->create()->id;
+        },
+        'age' => $fake->numberBetween(18, 100),
+        'gender' => 'M',
+        'birth_date' => date('Y-m-d'),
+//        'civil_status' => $fake->randomElements(['SI', 'MA', 'SE', 'DI', 'WI']),
+//        'address' => $fake->address,
+//        'place_of_birth' => $fake->city,
+//        'telephone_number' => $fake->phoneNumber,
+//        'mobile_number' => $fake->phoneNumber,
+//        'date_hired' => date('Y-m-d'),
+//        'date_end' => date('Y-m-d'),
+//        'sss_number' => $fake->numberBetween(1000000, 9999999),
+//        'phil_health_number' => $fake->numberBetween(1000000, 9999999),
+//        'pagibig_number' => $fake->numberBetween(1000000, 9999999),
+//        'tin_number' => $fake->numberBetween(1000000, 9999999),
+//        'account_number' => $fake->numberBetween(1000000, 9999999),
+//        'status' => 'Active',
     ];
 });
---
+++ database/migrations/2017_03_18_001428_create_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->smallInteger('age')->unsigned();
+            $table->date('birth_date');
+            $table->enum('gender', ['M', 'F']);
+
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
